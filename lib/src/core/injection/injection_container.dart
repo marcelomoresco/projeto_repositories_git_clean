1c1003ce5fd7dea2f3c96bc1169fe193f3d8bfb2
Marcelo Moresco <marcelo.moresco@valemobi.com.br>
2022-11-03 14:29:21 -0300

    feat: get_it injection into app

--- lib/src/core/injection/injection_container.dart
+++ lib/src/core/injection/injection_container.dart
@@ -1,5 +1,14 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/data/datasources/remote/firebase_datasource.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/data/datasources/remote/firebase_datasource_imp.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/data/repository/firebase_repository_implementation.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/domain/repository/firebase_repository.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/domain/usecases/sign_in_usecase.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/domain/usecases/sign_out_usecase.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/domain/usecases/sign_up_usecase.dart';
+import 'package:projeto_repositories_git_clean/src/features/login/presentation/cubits/user/user_login_cubit.dart';
 import 'package:projeto_repositories_git_clean/src/features/search/data/datasources/local/github_local_datasource_implementation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -23,22 +32,79 @@ import '../network/network_info_implementation.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //features
-
   //bloc
+
+  sl.registerFactory<UserLoginCubit>(
+    () => UserLoginCubit(
+      signInUsecase: sl(),
+      signUpUsecase: sl(),
+      signOutUsecase: sl(),
+    ),
+  );
+
   sl.registerFactory(
-      () => SearchBloc(getUserUseCase: sl(), getGitRepositoriesUsecase: sl()));
-  sl.registerFactory(() => FavoritesBloc(getFavoritesUsersUsecase: sl()));
-  sl.registerFactory(() => UsersBloc(getUsersByNameUsecase: sl()));
-  sl.registerFactory(() => RepositoriesBloc(getGitRepositoriesUsecase: sl()));
+    () => SearchBloc(
+      getUserUseCase: sl(),
+      getGitRepositoriesUsecase: sl(),
+    ),
+  );
+  sl.registerFactory(
+    () => FavoritesBloc(
+      getFavoritesUsersUsecase: sl(),
+    ),
+  );
+  sl.registerFactory(
+    () => UsersBloc(
+      getUsersByNameUsecase: sl(),
+    ),
+  );
+  sl.registerFactory(
+    () => RepositoriesBloc(
+      getGitRepositoriesUsecase: sl(),
+    ),
+  );
   //usecases
   sl.registerLazySingleton(
-      () => GetFavoritesUsersUsecase(githubRepository: sl()));
+    () => GetFavoritesUsersUsecase(
+      githubRepository: sl(),
+    ),
+  );
   sl.registerLazySingleton(
-      () => GetGitRepositoriesUsecase(githubRepository: sl()));
-  sl.registerLazySingleton(() => GetUserUsecase(githubRepository: sl()));
-  sl.registerLazySingleton(() => GetUsersByNameUsecase(githubRepository: sl()));
-  sl.registerLazySingleton(() => SaveUserUsecase(githubRepository: sl()));
+    () => GetGitRepositoriesUsecase(
+      githubRepository: sl(),
+    ),
+  );
+  sl.registerLazySingleton(
+    () => GetUserUsecase(
+      githubRepository: sl(),
+    ),
+  );
+  sl.registerLazySingleton(
+    () => GetUsersByNameUsecase(
+      githubRepository: sl(),
+    ),
+  );
+  sl.registerLazySingleton(
+    () => SaveUserUsecase(
+      githubRepository: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton(
+    () => SignInUsecase(
+      firebaseRepository: sl(),
+    ),
+  );
+  sl.registerLazySingleton(
+    () => SignOutUsecase(
+      firebaseRepository: sl(),
+    ),
+  );
+  sl.registerLazySingleton(
+    () => SignUpUsecase(
+      firebaseRepository: sl(),
+    ),
+  );
 
   //repository
   sl.registerLazySingleton<IGithubRepository>(
@@ -49,12 +115,24 @@ Future<void> init() async {
     ),
   );
 
+  sl.registerLazySingleton<FirebaseRepository>(
+    () => FirebaseRepositoryImplementation(
+      firebaseRemoteDatasource: sl(),
+    ),
+  );
+
   //datasource
   sl.registerLazySingleton<IGithubRemoteDataSource>(
       () => GithubRemoteDatasourceImplementation(client: sl()));
 
   sl.registerLazySingleton<IGithubLocalDataSource>(
       () => GithubLocalDataSourceImplementation(sharedPreferences: sl()));
+
+  sl.registerLazySingleton<IFirebaseRemoteDatasource>(
+    () => FirebaseDatasourceImplementation(
+      firebaseAuth: sl(),
+    ),
+  );
   //core
 
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImplementation(sl()));
@@ -62,8 +140,17 @@ Future<void> init() async {
   //external
 
   final sharedPreferences = await SharedPreferences.getInstance();
-  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(
+    () => sharedPreferences,
+  );
 
-  sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerLazySingleton(
+    () => http.Client(),
+  );
+  sl.registerLazySingleton(
+    () => DataConnectionChecker(),
+  );
+  sl.registerLazySingleton(
+    () => FirebaseAuth.instance,
+  );
 }
