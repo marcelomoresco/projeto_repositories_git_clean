daaab8d4b7083a187889d7e3c9f9ca8a6f389a1a
Marcelo Moresco <marcelo.moresco@valemobi.com.br>
2022-11-03 14:24:26 -0300

    feat: add firebase failure

--- lib/src/core/error/failures.dart
+++ lib/src/core/error/failures.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class Failure extends Equatable {
@@ -10,3 +11,10 @@ class ServerFailure extends Failure {}
 class CacheFailure extends Failure {}
 
 class OfflineFailure extends Failure {}
+
+class FirebaseFailure extends Failure {
+  final String? errorMessage;
+  FirebaseFailure(
+    this.errorMessage,
+  );
+}
