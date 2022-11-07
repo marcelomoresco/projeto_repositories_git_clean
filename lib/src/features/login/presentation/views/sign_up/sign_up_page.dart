b46a053c8bcea54878e07122a03c2a200b272d58
Marcelo Moresco <marcelomoresco0@gmail.com>
2022-11-06 23:49:13 -0300

    refactor: login module

--- lib/src/features/login/presentation/views/sign_up/sign_up_page.dart
+++ lib/src/features/login/presentation/views/sign_up/sign_up_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
